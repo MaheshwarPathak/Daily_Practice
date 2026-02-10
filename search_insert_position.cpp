e7a2946961a49131f66902e1d22a424e6d6b18f3
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-02-10 15:47:33 +0530

    DSA:solved Problem Search Insert Position in leetcode.

---
+++ search_insert_position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1,pos=nums.size();
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target){
+                pos=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return pos;
+    }
+};
