c8261b2602737fc4c4174d9de6800ec036ef838d
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-02-13 14:46:36 +0530

    DSA:Solved Problem Maximum Gap in leetcode.

---
+++ max_gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxGap=0;
+        for(int i=1;i<nums.size();i++){
+            int diff=nums[i]-nums[i-1];
+             if(diff>maxGap){
+                maxGap=diff;
+             }
+        }
+        return maxGap;
+    }
+};
