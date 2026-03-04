4a8f4ca7931f0217818f69191ce0aff158dfe441
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-03-04 08:38:10 +0530

    DSA:Solved Probelm Find Smallest Letter Greater Than the target.

---
+++ find_smallest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+         int low = 0; 
+        int high = letters.size()-1;
+        int pos = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(letters[mid] <= target)
+                low = mid + 1;
+            else{
+                high = mid - 1;
+                pos = high + 1;    
+            }    
+        }
+        if(pos == -1){
+            return letters[0];
+        }
+        return letters[pos];
+    }
+};
