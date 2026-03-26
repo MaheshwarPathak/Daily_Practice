16bd03b7987015561d33a0f4d3ab116c3432f02b
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-03-26 20:57:19 +0530

    DSA:Solved Problem Find Numbers with Even Number in Leetcode.

---
+++ find_num_with_even_num.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int Evencount=0;
+        for(int i=0;i<nums.size();i++
+        ){
+            int count=0;
+            int num=nums[i];
+            while(num>0){
+                num=num/10;
+                count++;
+            }
+            if(count%2==0){
+                Evencount++;
+            }
+        }
+        return Evencount;
+    }
+};
