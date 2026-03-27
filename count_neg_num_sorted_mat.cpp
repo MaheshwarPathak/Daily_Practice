f2051db378c3b39ac0f77d9201fa4ba9277c0057
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-03-27 23:15:46 +0530

    DSA:Solved Problem Count Negative Numbers in a Sorted Matrix in Leetcode.

---
+++ count_neg_num_sorted_mat.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count =0;
+        int  n = grid[0].size();
+        int m = grid.size();
+
+          for(int  i =0 ; i<m;i++){
+            for(int j =0; j<n ;j++){
+                 if(grid[i][j] < 0) count++;
+            }
+          }
+          return count;
+    }
+};
