31b1e9753e2b375fbc0a724de609dcc80e8a1dd6
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-04-14 00:51:06 +0530

    DSA:Solved Problem Smallest Index With Equal Value.

---
+++ smallest_idx_equal_val.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            if(i%10==nums[i]){
+                return i;
+                break;
+            }
+            i++;
+        }
+        return -1;
+    }
+};
