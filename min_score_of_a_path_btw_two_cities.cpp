2e257dd939db2f2a9ea2bc694446f3fa49579ab3
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-07-04 08:15:53 +0530

    DSA:Solved Problem Minimum Score Of a Path between two cities in Leetcode.

---
+++ min_score_of_a_path_btw_two_cities.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = INT_MAX;
+    void dfs(int node, vector<vector<pair<int,int>>>&adj, vector<int>&vis){
+        vis[node] = 1;
+        for(auto &it : adj[node]){
+            int next = it.first;
+            int wt = it.second;
+            ans = min(ans,wt);
+            if(!vis[next]){
+                dfs(next , adj , vis);
+            }
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto &it : roads){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        vector<int>vis(n + 1,0);
+        dfs(1, adj, vis);
+        return ans;
+    }
+};
