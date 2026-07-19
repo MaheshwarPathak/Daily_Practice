78a413c059950afefce680280412f4a3f95781e0
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-07-19 07:27:37 +0530

    DSA:Solved Problem Floor and Ceil.

---
+++ floor_and_ceil.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int findFloor(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int findCeil(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    pair<int, int> getFloorAndCeil(vector<int>& arr, int x) {
+        return {findFloor(arr, x), findCeil(arr, x)};
+    }
+};
