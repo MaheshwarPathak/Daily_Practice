a84efb1bf5b8650f2b48009f62839fcb9aeb1e6f
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-07-24 09:14:12 +0530

    DSA:Solved Problem Minimum Number Of Days to make m Bouquets in Leetcode.

---
+++ min_num_of_days_to_make_m_bouquets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPossible(vector<int>&bloomDay, int day, int m, int k){
+        int count = 0, bouquets = 0;
+        for(int bloom : bloomDay){
+            if(bloom <= day){
+                count++;
+                if(count == k){
+                    bouquets++;
+                    count = 0;
+                }
+            }else{
+                count = 0;
+            }
+        }
+        return bouquets >= m;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long totalFlowers = 1LL * m * k;
+        if(totalFlowers > bloomDay.size()) return -1;
+        int mini = *min_element(bloomDay.begin(), bloomDay.end());
+        int maxi = *max_element(bloomDay.begin(), bloomDay.end());
+
+        int low = mini, high = maxi, result = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(isPossible(bloomDay, mid, m, k)){
+                result = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
