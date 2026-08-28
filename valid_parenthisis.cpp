4d3b9aeb32d5e0439d3d40eb8d798f6ebb8debd3
Maheshwar Pathal <maheshwarpathak638@gmail.com>
2026-08-28 22:11:54 +0530

    DSA:Solved Problem Valid Parenthiesis in Leetcode.

---
+++ valid_parenthisis.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char ch : s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                char c = st.top();
+                st.pop();
+                if((ch == ')' && c == '(' ) || 
+                (ch == '}' && c == '{') || 
+                (ch == ']' && c == '[')){
+                    return true;
+                }else
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
