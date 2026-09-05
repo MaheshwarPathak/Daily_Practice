b7323934d36d0e7474051920a781b88bdd743304
Maheshwar Pathak <maheshwarpathak638@gmail.com>
2026-09-05 11:21:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (35.89%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long original = x;
+        long long ans;
+        while(x != 0){
+            int digit = x % 10;
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+        return (original == ans);
+    }
+};
