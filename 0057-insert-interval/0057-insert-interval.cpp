83fe7597978c18ac84a092cc9a272f51071e058f
Maheshwar Pathak <maheshwarpathak638@gmail.com>
2026-09-15 19:24:22 +0530

    Time: 4 ms (16.43%), Space: 21.6 MB (95.03%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // vector<vector<int>>ans;
+        // int n = intervals.size(), i = 0;
+        // while(i < n && intervals[i][1] < newInterval[0]){
+        //     ans.push_back(intervals[i]);
+        //     i++;
+        // }
+        // while(i < n && intervals[i][0] <= newInterval[1]){
+        //     newInterval[0] = min(newInterval[0], intervals[i][0]);
+        //     newInterval[1] = max(newInterval[1], intervals[i][1]);
+        //     i++;
+        // }
+        // ans.push_back(newInterval);
+        // while(i < n){
+        //     ans.push_back(intervals[i]);
+        //     i++;
+        // }
+        // return ans;
+        int n = intervals.size(), i = 0;
         vector<vector<int>>ans;
-        int n = intervals.size(),i = 0;
-        while(i < n && intervals[i][1] < newInterval[0]){
-            ans.push_back(intervals[i]);
-            i++;
-        }
-        while(i < n && intervals[i][0] <= newInterval[1]){
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        ans.push_back(newInterval);
-        while(i < n){
-            ans.push_back(intervals[i]);
-            i++;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        
+        for(int i = 0; i < intervals.size(); i++){
+            if(ans.empty() || ans.back()[1] < intervals[i][0])
+                ans.push_back(intervals[i]);
+            else
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]); 
         }
         return ans;
     }
